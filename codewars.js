87906dc826f1cf55d96cd72e2e84d229bae974fa
Volkan <volkan.guven@hotmail.se>
2019-11-01 23:08:15 +0100

    Added link to kata

--- codewars.js
+++ codewars.js
@@ -1,4 +1,8 @@
 /*
+  Codewars link:
+  https://www.codewars.com/kata/595970246c9b8fa0a8000086
+
    Capitilize first character of the word and return the final output.
 */
+
 const capitalizeWord = (word) => word.replace(/^\w/, (char) => {return char.toUpperCase()})
